4ffe10506a20810faaa2229c4a0760761fb6b3bb
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2022-11-24 23:30:54 -0600

    Added Date & Time

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,18 @@
+var timeDisplayEl = $('#time-display');
+
+function displayTime() {
+    var rightNow = dayjs().format('dddd, MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+displayTime();
+setInterval(displayTime, 1000);
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+    
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
