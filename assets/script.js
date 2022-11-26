18a7cd0f2764629f233db208b84617b3e9a5f68c
Brandon <110788407+bkorbsquare@users.noreply.github.com>
2022-11-26 17:54:31 -0600

    Attempt 1 at coloring timeblocks

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,29 @@
 var timeDisplayEl = $('#time-display');
+var hour9 = $('#hour-9');
+var hour10 = $('#hour-10');
+var hour11 = $('#hour-11');
+var hour12 = $('#hour-12');
+var hour13 = $('#hour-13');
+var hour14 = $('#hour-14');
+var hour15 = $('#hour-15');
+var hour16 = $('#hour-16');
+var hour17 = $('#hour-17');
+
+function colorTimeblocks() {
+  if (dayjs().format('hh') === 09) {
+    hour9.color = "Red";
+  }
+  else if (dayjs().format('hh') > 09) {
+    hour9.color = "Grey";
+  }
+  else {
+    hour9.color = "Green";
+  }
+
+}
 
 function displayTime() {
-    var rightNow = dayjs().format('dddd, MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
+    timeDisplayEl.text(dayjs().format('dddd, MMM DD, YYYY [at] hh:mm:ss a'));
   }
 
 displayTime();
